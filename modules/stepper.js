de26b31963ef267fc438a0f596e5d8a9f670c92b
Jamie <jamie@lambertstock.com>
2021-10-20 00:30:11 +0100

    Working movement and words

---
+++ modules/stepper.js
@@ -0,0 +1,109 @@
+const { Board, Stepper } = require('johnny-five');
+
+const board = new Board();
+
+// Current location of pointer on board
+let locationX = 0;
+let locationY = 0;
+
+board.on('ready', () => {
+  // Setup stepper Y
+  const stepperY = new Stepper({
+    type: Stepper.TYPE.DRIVER,
+    stepsPerRev: 200,
+    pins: [3, 6],
+  });
+
+  // Setup stepper X
+  const stepperX = new Stepper({
+    type: Stepper.TYPE.DRIVER,
+    stepsPerRev: 200,
+    pins: [2, 5],
+  });
+
+  // Set stepper to 180 RPM with acceleration and deceleration
+  stepperY.rpm(180).accel(1600).decel(1600);
+  stepperX.rpm(180).accel(1600).decel(1600);
+
+  module.exports.reset = () => {
+    locationX = 0;
+    locationY = 0;
+    console.log('X & Y Reset');
+  };
+
+  module.exports.stepperY = stepperY;
+  module.exports.stepperX = stepperX;
+
+  module.exports.move = (options) => {
+    const running = new Promise((resolve) => {
+      let bothComplete = 0;
+      const done = () => {
+        bothComplete += 1;
+        console.log(bothComplete);
+        if (bothComplete === 2) {
+          console.log('Word done');
+          resolve();
+        }
+      };
+
+      stepperY.step({
+        steps: options?.Y?.steps || 0,
+        direction: options?.Y?.dir,
+      }, () => done());
+
+      stepperX.step({
+        steps: options?.X?.steps || 0,
+        direction: options?.X?.dir,
+      }, () => done());
+    });
+    return running;
+  };
+
+  module.exports.letter = (data) => {
+    // the final array of board intructions
+    const wordArray = [];
+
+    // Loop through each word and work out what needs to be done
+    for (let i = 0; i < data.length; i += 1) {
+      const amountLeft = {
+        X: { amount: 0, dir: 'right' },
+        Y: { amount: 0, dir: 'up' },
+      };
+
+      // Work out the X (LEFT/RIGHT)
+      if (locationX < data[i].X) {
+        amountLeft.X = {
+          amount: data[i].X - locationX,
+          dir: 0,
+        };
+        locationX += amountLeft.X.amount;
+      } else if (locationX > data[i].X) {
+        amountLeft.X = {
+          amount: locationX - data[i].X,
+          dir: 1,
+        };
+        locationX -= amountLeft.X.amount;
+      }
+
+      // Work out the Y (UP/DOWN)
+      if (locationY < data[i].Y) {
+        amountLeft.Y = {
+          amount: data[i].Y - locationY,
+          dir: 1,
+        };
+        locationY += amountLeft.Y.amount;
+      } else if (locationY > data[i].Y) {
+        amountLeft.Y = {
+          amount: locationY - data[i].Y,
+          dir: 0,
+        };
+        locationY -= amountLeft.Y.amount;
+      }
+
+      // Push into our array
+      wordArray.push(amountLeft);
+    }
+    // return array
+    return wordArray;
+  };
+});
--- routes/boardRoute.js
+++ routes/boardRoute.js
@@ -1,16 +1,9 @@
 const express = require('express');
 
 const { getCharacters } = require('../modules/dbQueries');
-const { Stepper, board } = require('../modules/classStepper');
+const stepper = require('../modules/stepper');
 
 const router = express.Router();
-
-// setup new stepper
-const stepper = new Stepper({
-  stepPin: { X: 2, Y: 3 },
-  dirPin: { X: 5, Y: 6 },
-  stepsPerRev: { X: 3600, Y: 3600 },
-});
 
 // Helper Function
 const checkAuth = (req, res, next) => {
@@ -25,37 +18,64 @@ const sendStatus = (res, statusCode, statusMessage) => {
   });
 };
 
-board.on('ready', () => {
-  // BOARD INIT
-  router.post('/init', checkAuth, async (req, res) => {
-    await stepper.init();
-    sendStatus(res, 200, 'Success');
+router.post('/init', checkAuth, async (req, res) => {
+  await stepper.move({
+    X: { dir: 1, steps: 18600 },
+    Y: { dir: 0, steps: 11350 },
+  });
+  stepper.reset();
+  sendStatus(res, 200, 'Success');
+});
+
+// Word handler
+router.post('/word', checkAuth, async (req, res) => {
+  // Make array out of word
+  const wordArray = Array.from(req?.body?.word.toLowerCase());
+
+  // Get list db entries for matches
+  const allCharacters = await getCharacters();
+
+  const charArray = [];
+
+  wordArray.forEach(async (char) => {
+    // Find letter in DB
+    const letter = allCharacters.find((l) => l.CHARACTER === char);
+
+    // Add to array
+    charArray.push(letter);
   });
 
-  // Word handler
-  router.post('/word', checkAuth, async (req, res) => {
-    // Make array out of word
-    const wordArray = Array.from(req?.body?.word.toLowerCase());
+  // Send to stepper
+  const decodedWord = stepper.letter(charArray);
 
-    // Get list db entries for matches
-    const allCharacters = await getCharacters();
+  let i = 0;
+  let running = false;
 
-    const charArray = [];
+  const id = setInterval(() => {
+    // If word is complete, exit
+    if (i === decodedWord.length - 1) clearInterval(id);
 
-    wordArray.forEach(async (char) => {
-      // Find letter in DB
-      const letter = allCharacters.find((l) => l.CHARACTER === char);
+    // If board is still running, ignore
+    if (running) return;
 
-      // Add to array
-      charArray.push(letter);
+    // Set out board as running
+    running = true;
+
+    // Move our steppers to letter
+    stepper.move({
+      X: { dir: decodedWord[i].X.dir, steps: decodedWord[i].X.amount },
+      Y: { dir: decodedWord[i].Y.dir, steps: decodedWord[i].Y.amount },
+    }).then(() => {
+      // Once completed letter, wait 5 secs then move to next one
+      setTimeout(() => {
+        i += 1;
+        running = false;
+      }, 5000);
     });
+  }, 1000);
 
-    // Send to stepper
-    stepper.letter(charArray);
-
-    // 200: Word processed
-    sendStatus(res, 200, 'Success');
-  });
+  // 200: Word processed
+  sendStatus(res, 200, 'Success');
 });
 
 module.exports = router;
